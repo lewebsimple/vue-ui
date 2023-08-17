579c0c16fa6e70b0314d5a000a94aa3dfd5c5ce0
Pascal Martineau <pascal@lewebsimple.ca>
2023-08-17 10:51:37 -0400

    feat: Initial Vite project

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "./src/index.ts"),
+      name: "vue-ui",
+      formats: ["es"],
+      fileName: (format) => `vue-ui.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        exports: "named",
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+    emptyOutDir: false,
+  },
+});
